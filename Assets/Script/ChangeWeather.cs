4136faa1fb104bf134e1cf8a70656992b7f7c0cd
corinnedaigle3 <corinnedaigle9@gmail.com>
2025-11-13 13:54:05 -0500

    New input system

--- Assets/Script/ChangeWeather.cs
+++ Assets/Script/ChangeWeather.cs
@@ -1,18 +1,64 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ChangeWeather : MonoBehaviour
 {
+    [Header("Skybox")]
     /*public Material orlandoSkybox;
     public Material parisSkybox;
     public Material tokyoSkybox;
     public Material athensSkybox;
     public Material stockholmSkybox;*/
-    public WeatherManager m;
 
+    [Header("Light")]
+    public Light directionalLight;
     public Color lightColor;
+    public float targetIntensity;
     public Vector3 lightRotation;
 
-    public Light directionalLight;
+    public WeatherManager m;
+
+    [Header("Input Actions")]
+    private InputSystem_Actions actions;
+    private InputAction orlandoWeather;
+    private InputAction parisWeather;
+    private InputAction tokyoWeather;
+    private InputAction athensWeather;
+    private InputAction stockholmWeather;
+
+    void Awake()
+    {
+        actions = new InputSystem_Actions();
+        orlandoWeather = actions.Player.Orlando;
+        parisWeather = actions.Player.Paris;
+        tokyoWeather = actions.Player.Tokyo;
+        athensWeather = actions.Player.Athens;
+        stockholmWeather = actions.Player.Stockholm;
+    }
+
+    void OnEnable()
+    {
+        actions.Player.Enable(); // Enable the Action Map
+
+        // Subscribe to the performed event
+        orlandoWeather.performed += ChangeToOrlandoSkybox;
+        parisWeather.performed += ChangeToParisSkybox;
+        tokyoWeather.performed += ChangeToTokyoSkybox;
+        athensWeather.performed += ChangeToAthensSkybox;
+        stockholmWeather.performed += ChangeToStockholmSkybox;
+    }
+
+    void OnDisable()
+    {
+        // Unsubscribe from the event
+        orlandoWeather.performed -= ChangeToOrlandoSkybox;
+        parisWeather.performed -= ChangeToParisSkybox;
+        tokyoWeather.performed -= ChangeToTokyoSkybox;
+        athensWeather.performed -= ChangeToAthensSkybox;
+        stockholmWeather.performed -= ChangeToStockholmSkybox;
+
+        actions.Player.Disable(); // Disable the Action Map
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,8 +66,19 @@ public class ChangeWeather : MonoBehaviour
         m = new WeatherManager();
         StartCoroutine(m.GetWeatherXML_1(m.OnXMLDataLoaded));
         
-        //directionalLight = GetComponent<Light>();
+        directionalLight = GetComponent<Light>();
         //RenderSettings.skybox = orlandoSkybox;
+
+        if (directionalLight != null)
+        {
+            // Set the initial intensity
+            directionalLight.intensity = targetIntensity;
+            Debug.Log("Directional Light intensity set to: " + directionalLight.intensity);
+        }
+        else
+        {
+            Debug.LogError("Directional Light not found or assigned!");
+        }
     }
     void Update()
     {
@@ -30,32 +87,32 @@ public class ChangeWeather : MonoBehaviour
         //ChangeToOrlandoSkybox();
     }
 
-    public void ChangeToOrlandoSkybox()
+    public void ChangeToOrlandoSkybox(InputAction.CallbackContext context)
     {
         //RenderSettings.skybox = orlandoSkybox;
         directionalLight.color = Color.blue;
 
     }
 
-    public void ChangeToParisSkybox()
+    public void ChangeToParisSkybox(InputAction.CallbackContext context)
     {
         //RenderSettings.skybox = parisSkybox;
         directionalLight.color = Color.blue;
     }
 
-    public void ChangeToTokyoSkybox()
+    public void ChangeToTokyoSkybox(InputAction.CallbackContext context)
     {
         //RenderSettings.skybox = tokyoSkybox;
         directionalLight.color = Color.blue;
     }
 
-    public void ChangeToAthensSkybox()
+    public void ChangeToAthensSkybox(InputAction.CallbackContext context)
     {
         //RenderSettings.skybox = athensSkybox;
         directionalLight.color = Color.blue;
     }
 
-    public void ChangeToStockholmSkybo4()
+    public void ChangeToStockholmSkybox(InputAction.CallbackContext context)
     {
         //RenderSettings.skybox = stockholmSkybox;
         directionalLight.color = Color.blue;
