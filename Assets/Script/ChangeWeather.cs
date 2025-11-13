3084c0c702a6c825bd5069ca648e3842d2d4e8da
corinnedaigle3 <corinnedaigle9@gmail.com>
2025-11-13 15:29:04 -0500

    Updated ChangeWeather

--- Assets/Script/ChangeWeather.cs
+++ Assets/Script/ChangeWeather.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -15,15 +16,14 @@ public class ChangeWeather : MonoBehaviour
     public CityState cityState;
 
     [Header("Skybox")]
-    /*public Material orlandoSkybox;
-    public Material parisSkybox;
-    public Material tokyoSkybox;
-    public Material athensSkybox;
-    public Material stockholmSkybox;*/
+    public Material sunnySkybox;
+    public Material cloudySkybox;
+    public Material rainySkybox;
+    public Material snowySkybox;
 
     [Header("Light")]
-    //public Light directionalLight;
-    //public float targetIntensity;
+    public Light directionalLight;
+    public float targetIntensity;
     //public Vector3 lightRotation;
 
     public WeatherManager m;
@@ -33,9 +33,7 @@ public class ChangeWeather : MonoBehaviour
     {
         m = new WeatherManager();
         StartCoroutine(m.GetWeatherXML_1(m.OnXMLDataLoaded));
-        
-        //directionalLight = GetComponent<Light>();
-        //RenderSettings.skybox = orlandoSkybox;
+        directionalLight = Object.FindFirstObjectByType<Light>();
     }
 
     void Update()
@@ -50,28 +48,47 @@ public class ChangeWeather : MonoBehaviour
             case CityState.Orlando:
                 Debug.Log("Orlando");
                 StartCoroutine(m.GetWeatherXML_1(m.OnXMLDataLoaded));
-                //RenderSettings.skybox = orlandoSkybox;
+                RenderSettings.skybox = sunnySkybox;
                 break;
+
             case CityState.Paris:
                 Debug.Log("Paris");
                 StartCoroutine(m.GetWeatherXML_2(m.OnXMLDataLoaded));
-                //RenderSettings.skybox = parisSkybox;
+                RenderSettings.skybox = rainySkybox;
                 break;
+
             case CityState.Tokyo:
                 Debug.Log("Tokyo");
                 StartCoroutine(m.GetWeatherXML_3(m.OnXMLDataLoaded));
-                //RenderSettings.skybox = tokyoSkybox;
+                RenderSettings.skybox = cloudySkybox;
                 break;
+
             case CityState.Athens:
                 Debug.Log("Athens");
                 StartCoroutine(m.GetWeatherXML_4(m.OnXMLDataLoaded));
-                //RenderSettings.skybox = athensSkybox;
+                RenderSettings.skybox = sunnySkybox;
                 break;
+
             case CityState.Stockholm:
                 Debug.Log("Stockholm");
                 StartCoroutine(m.GetWeatherXML_5(m.OnXMLDataLoaded));
-                //RenderSettings.skybox = stockholmSkybox;
+                RenderSettings.skybox = snowySkybox;
                 break;
         }
+    }
+
+    IEnumerator LerpLightIntensity(float targetIntensity, float duration = 1f)
+    {
+        float startIntensity = directionalLight.intensity;
+        float time = 0f;
+
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            directionalLight.intensity = Mathf.Lerp(startIntensity, targetIntensity, time / duration);
+            yield return null;
+        }
+
+        directionalLight.intensity = targetIntensity;
     }
 }
